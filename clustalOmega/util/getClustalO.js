be6373a4d031d5276d28004ef2c938dcb515dc7e
DinikaSen <dinikasen@gmail.com>
2018-02-09 23:26:58 +0530

    Excecute command added

--- clustalOmega/util/getClustalO.js
+++ clustalOmega/util/getClustalO.js
@@ -4,7 +4,7 @@
 const os = require('os');
 const download = require('download-file');
 const fs = require('fs');
-const child_process = require('child_process');
+var child_process = require('child_process');
 
 var address = 'http://www.clustal.org/omega/';
 var platform = os.platform();
@@ -43,98 +43,35 @@ switch (platform) {
 function downloadClustalOmega(url) {
     console.log('Downloading Clustal Omega from ',url);
     download(url,{directory:'./clustalOmega/bin',filename:'clustalo'},function(err) {
-            if (err) {
-                console.log('Download failed');
+        if (err) {
+            console.log('Download failed');
+            console.log(err);
+        }
+        else {
+            console.log('Download complete');
+            //makeExcecutable();
+            }
+    });
+}
+
+//Debug
+function makeExcecutable(){
+    if(platform=='linux' || platform=='freebsd'){
+        child_process.exec('chmod u+x clustalo',{cwd:'../bin'},function (err) {
+            if(err){
+                console.log('ERROR: ' + err);
+            }else{
+                console.log('done')
+            }
+        })
+        ;
+    }else if(platform=='darwin'){
+        child_process.exec('chmod 755 clustalo',{cwd:'../bin'},function (err) {
+            if(err){
                 console.log(err);
             }
-            else {
-                console.log('Download complete');
-
-                if(platform=='linux' || platform=='freebsd'){
-                    child_process.exec('chmod u+x clustalo',{cwd:'../bin'},function (err) {
-                        if(err){
-                            console.log('MAKING EXEC. ERROR: ' + err);
-                        }else{
-                            console.log('done')
-                        }
-                    })
-                    ;
-                }else if(platform=='darwin'){
-                    child_process.exec('chmod 755 clustalo',{cwd:'../bin'},function (err) {
-                        if(err){
-                            console.log(err);
-                        }
-                    });
-                }else if(platform=='windows'){
-                    //TODO AFTER FIGURING OUT A WAY TO BUILD EXEC.
-                }
-            }
-        }
-    );
-}
-/*
-function downloadClustalOmega(url){
-    console.log('Downloading Clustal Omega from ',url);
-    download(url,'/Desktop',{filename:'clustalo'});
-    console.log('Download complete');
-}
-*/
-/*
-var OS = require('os');
-var Download = require('download');
-var Client = require('ftp');
-
-var tt = 'ftp.ncbi.nlm.nih.gov';
-var address = '/blast/executables/blast+/LATEST/';
-
-var platform = OS.platform();
-var arch = OS.arch();
-
-if (platform === 'darwin') {
-    platform = 'macosx';
-    arch = 'universal';
-}
-
-var foundIt = false;
-var fileName = '';
-
-console.log('looking for', platform, arch, '...');
-
-var c = new Client();
-c.on('ready', function () {
-    c.list(address, function (err, list) {
-        if (err) throw err;
-        //console.dir(list);
-
-
-        list.forEach(function (l) {
-            if (l.name.indexOf(platform) > -1) {
-                if (l.name.indexOf(arch) > -1) {
-                    if (l.name.indexOf('md5') === -1) {
-                        foundIt = true;
-                        fileName = l.name;
-                    }
-                }
-            }
         });
-        if (!foundIt) {
-            console.error('we could not find the correct version of blast+ for you, sorry');
-        } else {
-
-            var finalURL = tt + address + fileName;
-            downloadIt(finalURL);
-        }
-        c.end();
-    });
-});
-c.connect({host: tt});
-
-
-function downloadIt(url) {
-    console.log('Downloading', url, '...');
-    new Download({mode: '755', extract: true})
-        .get('http://' + url) //have to add http to url
-        .dest('bin')
-        .run();
+    }else if(platform=='windows'){
+        //TODO AFTER FIGURING OUT A WAY TO BUILD EXEC.
+    }
 }
-*/
